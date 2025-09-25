b8280b042c426f7aeb3c685a8a73e98d397d9e08
Manuel Candales <mcandales@meta.com>
2025-09-25 12:24:19 -0400

    refactor ops

--- backends/apple/metal/runtime/shims/shim_mps.h
+++ backends/apple/metal/runtime/shims/shim_mps.h
@@ -25,46 +25,6 @@ using AOTIMetalShaderLibraryHandle = AOTIMetalShaderLibraryOpaque*;
 extern "C" {
 #endif
 
-/**
- * ExecutorTorch implementation of aoti_torch_mps_addmm_out.
- * Performs matrix multiplication with bias: out = beta * self + alpha * (mat1 @ mat2)
- */
-AOTITorchError aoti_torch_mps_addmm_out(
-    AOTITensorHandle out,
-    AOTITensorHandle self,
-    AOTITensorHandle mat1,
-    AOTITensorHandle mat2,
-    double beta,
-    double alpha);
-
-/**
- * ExecutorTorch implementation of aoti_torch_mps_mm_out.
- * Performs simple matrix multiplication: out = self @ mat2
- */
-AOTITorchError aoti_torch_mps_mm_out(
-    AOTITensorHandle out,
-    AOTITensorHandle self,
-    AOTITensorHandle mat2);
-
-/**
- * ExecutorTorch implementation of aoti_torch_mps_convolution.
- * Performs 2D convolution operation - matches PyTorch AOTI signature
- */
-AOTITorchError aoti_torch_mps_convolution(
-    AOTITensorHandle input,
-    AOTITensorHandle weight,
-    AOTITensorHandle* bias,
-    const int64_t* stride,
-    int64_t stride_len_,
-    const int64_t* padding,
-    int64_t padding_len_,
-    const int64_t* dilation,
-    int64_t dilation_len_,
-    int32_t transposed,
-    const int64_t* output_padding,
-    int64_t output_padding_len_,
-    int64_t groups,
-    AOTITensorHandle* ret0);
 
 // MetalShaderLibrary functions
 AOTITorchError aoti_torch_mps_create_shader_library(
